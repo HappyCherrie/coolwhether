f2d089fffa87a72bfb1484d453710663d8c34cdb
Cherrie <Cherrie>
2015-12-15 13:32:07 +0800

    数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,41 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by ct on 2015/12/14.
+ */
+public class Province
+{
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public getProvinceName()
+    {
+        return provinceName;
+    }
+
+    public void setProvinceName(int provinceName)
+    {
+        this.provinceName = provinceName;
+    }
+
+    public getProvinceCode()
+    {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode)
+    {
+        this.provinceCode = provinceCode;
+    }
+}
